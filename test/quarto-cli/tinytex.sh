972a714edcd7d124581ed4fffab91b0207800f6c
eitsupi <ts1s1andn@gmail.com>
2022-09-25 13:49:08 +0000

    fix test

--- test/quarto-cli/tinytex.sh
+++ test/quarto-cli/tinytex.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Feature-specific tests
-check "check for quarto" quarto check all
+check "version" quarto check all
 
 # Report result
 reportResults
