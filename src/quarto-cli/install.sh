0325931c2200585603f8c88227d13c34d2136305
eitsupi <ts1s1andn@gmail.com>
2022-09-25 13:26:54 +0000

    add new option to install tinytex

--- src/quarto-cli/install.sh
+++ src/quarto-cli/install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 CLI_VERSION=${VERSION:-"latest"}
+INSTALL_TINYTEX=${INSTALL_TINYTEXT-"false"}
+
+USERNAME=${USERNAME:-"automatic"}
 
 set -e
 
@@ -15,6 +18,23 @@ architecture="$(dpkg --print-architecture)"
 if [ "${architecture}" != "amd64" ]; then
     echo "(!) Architecture $architecture unsupported"
     exit 1
+fi
+
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u "${CURRENT_USER}" > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+    USERNAME=root
 fi
 
 apt_get_update() {
@@ -98,5 +118,10 @@ fi
 echo "Downloading Quarto CLI..."
 
 install
+
+if [ "${INSTALL_TINYTEX}" = "true" ]; then
+    echo "Installing TinyTeX..."
+    su ${USERNAME} -c 'quarto tools install tinytex'
+fi
 
 echo "Done!"
