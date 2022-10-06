2c6369315a822baa59267c0677ce96c35099e8da
eitsupi <ts1s1andn@gmail.com>
2022-10-06 12:53:42 +0000

    Revert "fix install command"
    This reverts commit 837674ef62ae6347c0ae3545983202b592edb7cc.

--- src/quarto-cli/install.sh
+++ src/quarto-cli/install.sh
@@ -125,7 +125,7 @@ install_cli "${CLI_VERSION}"
 
 if [ "${INSTALL_TINYTEX}" = "true" ]; then
     echo "Installing TinyTeX..."
-    su ${USERNAME} -c 'quarto install tool tinytex'
+    su ${USERNAME} -c 'quarto tools install tinytex'
 fi
 
 echo "Done!"
