bc1dbb1e276f455154d164fa0b4dce41988af488
eitsupi <ts1s1andn@gmail.com>
2022-10-12 09:29:10 +0000

    update path

--- test/r-rig/rpackages-source-install.sh
+++ test/r-rig/rpackages-source-install.sh
@@ -12,8 +12,8 @@ check "httpgd" R -q -e 'names(installed.packages()[, 3])' | grep httpgd
 check "devtools" R -q -e 'names(installed.packages()[, 3])' | grep devtools
 check "vscDebugger" R -q -e 'names(installed.packages()[, 3])' | grep vscDebugger
 check "rmarkdown" R -q -e 'rmarkdown::pandoc_version()'
-check "jupyter" /root/.local/bin/jupyter kernelspec list | grep jupyter/kernels/ir
-check "radian" /root/.local/bin/radian --version
+check "jupyter" jupyter kernelspec list | grep jupyter/kernels/ir
+check "radian" radian --version
 
 # Report result
 reportResults
