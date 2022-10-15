5dda502d61a8162b56919e205b591359830021a6
eitsupi <ts1s1andn@gmail.com>
2022-10-15 14:03:14 +0000

    add options

--- src/r-apt/install.sh
+++ src/r-apt/install.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+VSCODE_R_SUPPORT=${VSCODERSUPPORT:-"minimal"}
+INSTALL_DEVTOOLS=${INSTALLDEVTOOLS:-"false"}
+INSTALL_RMARKDOWN=${INSTALLRMARKDOWN:-"false"}
+INSTALL_JUPYTERLAB=${INSTALLJUPYTERLAB:-"false"}
+INSTALL_RADIAN=${INSTALLRADIAN:-"false"}
+INSTALL_VSCDEBUGGER=${INSTALLVSCDEBUGGER:-"false"}
+
 USERNAME=${USERNAME:-"automatic"}
+
+APT_PACKAGES=(r-base)
+PIP_PACKAGES=()
 
 set -e
 
@@ -35,6 +45,32 @@ elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} >/dev/null 2>&1; then
     USERNAME=root
 fi
 
+# Check options for installing packages
+if [ "${VSCODE_R_SUPPORT}" = "minimal" ]; then
+    APT_PACKAGES+=(r-cran-jsonlite r-cran-rlang)
+elif [ "${VSCODE_R_SUPPORT}" = "lsp" ] || [ "${VSCODE_R_SUPPORT}" = "languageserver" ]; then
+    APT_PACKAGES+=(r-cran-languageserver)
+elif [ "${VSCODE_R_SUPPORT}" = "full" ]; then
+    APT_PACKAGES+=(r-cran-languageserver r-cran-httpgd)
+fi
+
+if [ "${INSTALL_DEVTOOLS}" = "true" ]; then
+    APT_PACKAGES+=(r-cran-devtools)
+fi
+
+if [ "${INSTALL_RMARKDOWN}" = "true" ]; then
+    APT_PACKAGES+=(r-cran-rmarkdown)
+fi
+
+if [ "${INSTALL_RADIAN}" = "true" ]; then
+    PIP_PACKAGES+=(radian)
+fi
+
+if [ "${INSTALL_JUPYTERLAB}" = "true" ]; then
+    APT_PACKAGES+=(r-cran-irkernel)
+    PIP_PACKAGES+=(jupyterlab)
+fi
+
 apt_get_update() {
     if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
         echo "Running apt-get update..."
@@ -47,6 +83,21 @@ check_packages() {
     if ! dpkg -s "$@" >/dev/null 2>&1; then
         apt_get_update
         apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+check_pip() {
+    if ! python3 -m pip --help >/dev/null 2>&1; then
+        check_packages python3-pip python3-dev
+    fi
+}
+
+install_pip_packages() {
+    packages="$*"
+    if [ -n "${packages}" ]; then
+        check_pip
+        # shellcheck disable=SC2086
+        python3 -m pip install --upgrade --no-cache-dir --no-warn-script-location ${packages}
     fi
 }
 
@@ -68,9 +119,25 @@ elif grep -q "Debian" </etc/os-release; then
 fi
 
 apt-get update -y
-apt-get -y install --no-install-recommends r-base
+# shellcheck disable=SC2048 disable=SC2086
+check_packages ${APT_PACKAGES[*]}
+# shellcheck disable=SC2048 disable=SC2086
+install_pip_packages ${PIP_PACKAGES[*]}
+
+if [ "${INSTALL_VSCDEBUGGER}" = "true" ]; then
+    check_packages git make r-base-dev r-cran-remotes r-cran-r6 r-cran-jsonlite
+    R -q -e "remotes::install_github('ManuelHentschel/vscDebugger@$(git ls-remote --tags https://github.com/ManuelHentschel/vscDebugger | grep -oP "v[0-9]+\\.[0-9]+\\.[0-9]+" | sort -V | tail -n 1)')"
+fi
+
+# Set up IRkernel
+if [ "${INSTALL_JUPYTERLAB}" = "true" ]; then
+    echo "Register IRkernel..."
+    # shellcheck disable=SC2016
+    su ${USERNAME} -c 'export PATH="/home/'${USERNAME}'/.local/bin:/root/.local/bin:${PATH}"; R -q -e "IRkernel::installspec()"'
+fi
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
+rm -rf /tmp/Rtmp*
 
 echo "Done!"
