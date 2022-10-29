56e3e8e4f3a2fa2876cc8bdea18eea4c94318a21
eitsupi <ts1s1andn@gmail.com>
2022-10-29 12:41:13 +0000

    add test

---
+++ test/r-apt/all-on-unstable-on-stable.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+check "R" R -q -e "sessionInfo()"
+check "languageserver" R -q -e 'names(installed.packages()[, 3])' | grep languageserver
+check "httpgd" R -q -e 'names(installed.packages()[, 3])' | grep httpgd
+check "devtools" R -q -e 'names(installed.packages()[, 3])' | grep devtools
+check "vscDebugger" R -q -e 'names(installed.packages()[, 3])' | grep vscDebugger
+check "rmarkdown" R -q -e 'rmarkdown::pandoc_version()'
+check "jupyter" jupyter kernelspec list | grep jupyter/kernels/ir
+check "radian" radian --version
+
+# Report result
+reportResults
