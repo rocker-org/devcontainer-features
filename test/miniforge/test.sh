695f7c36a4b72610c109fe6166bb14ff3ba4a36e
eitsupi <ts1s1andn@gmail.com>
2022-10-30 10:36:46 +0000

    fix version pattern

--- test/miniforge/test.sh
+++ test/miniforge/test.sh
@@ -20,7 +20,7 @@ check_packages() {
 }
 
 check_packages git ca-certificates
-LATEST_VERSION="$(git ls-remote --tags https://github.com/conda-forge/miniforge | grep -oP "[0-9]+\\.[0-9]+.[0-9]-[0-9]+" | sort -V | tail -n 1)"
+LATEST_VERSION="$(git ls-remote --tags https://github.com/conda-forge/miniforge | grep -oP "[0-9]+\\.[0-9]+.[0-9]+" | sort -V | tail -n 1)"
 
 # shellcheck source=/dev/null
 source dev-container-features-test-lib
