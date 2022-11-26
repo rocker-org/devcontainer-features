7ad6c45dd4ae1e7cc1e87ce53f1eb8ae5dc7248d
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-11-26 17:46:14 +0900

    [r-rig] Add a new option to install renv (#101)

--- test/r-rig/rpackages-source-install.sh
+++ test/r-rig/rpackages-source-install.sh
@@ -10,6 +10,7 @@ check "R" R -q -e "sessionInfo()"
 check "languageserver" R -q -e 'names(installed.packages()[, 3])' | grep languageserver
 check "httpgd" R -q -e 'names(installed.packages()[, 3])' | grep httpgd
 check "devtools" R -q -e 'names(installed.packages()[, 3])' | grep devtools
+check "renv" R -q -e 'names(installed.packages()[, 3])' | grep renv
 check "vscDebugger" R -q -e 'names(installed.packages()[, 3])' | grep vscDebugger
 check "rmarkdown" R -q -e 'rmarkdown::pandoc_version()'
 check "jupyter" jupyter kernelspec list | grep jupyter/kernels/ir
