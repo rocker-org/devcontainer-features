ae083ae98a50219c41a023401c708e703ae8351f
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-12-12 14:24:48 +0000

    ensure run as root user

--- src/apt-packages/install.sh
+++ src/apt-packages/install.sh
@@ -10,6 +10,11 @@ set -e
 # Clean up
 rm -rf /var/lib/apt/lists/*
 
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
 apt_get_update() {
     if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
         echo "Running apt-get update..."
