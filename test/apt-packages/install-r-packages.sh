daa024603f2b4771e323c4cf4b3319df755c6387
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-12-12 14:17:35 +0000

    add tests

---
+++ test/apt-packages/install-r-packages.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+check "R curl package" R -q -e 'names(installed.packages()[, 3])' | grep curl
+check "R dplyr package" R -q -e 'names(installed.packages()[, 3])' | grep dplyr
+
+# Report result
+reportResults
