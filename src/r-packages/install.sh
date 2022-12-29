ad23cbc42362a3675b06bf99ecf77b54bdd0216a
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-12-29 15:53:10 +0000

    Improve checking

--- src/r-packages/install.sh
+++ src/r-packages/install.sh
@@ -10,14 +10,19 @@ R_PACKAGES=("${PACKAGES//,/ }")
 
 set -e
 
+if [ -z "${PACKAGES}" ]; then
+    echo "No R packages specified. Skip installation..."
+    exit 0
+fi
+
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
 
-if [ -z "${PACKAGES}" ]; then
-    echo "No R packages specified. Skip installation..."
-    exit 0
+if [ ! -x "$(command -v R)" ]; then
+    echo "Cannot run R. Set R before installing this Feature..."
+    exit 1
 fi
 
 install_pak() {
