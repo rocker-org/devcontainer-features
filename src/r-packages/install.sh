8d910e3666594bed0d22103ce0490f280662f8a2
eitsupi <ts1s1andn@gmail.com>
2022-12-30 11:27:21 +0900

    fix for USERNAME=automatic

--- src/r-packages/install.sh
+++ src/r-packages/install.sh
@@ -25,6 +25,23 @@ if [ ! -x "$(command -v R)" ]; then
     exit 1
 fi
 
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u "${CURRENT_USER}" >/dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u "${USERNAME}" >/dev/null 2>&1; then
+    USERNAME=root
+fi
+
 install_pak() {
     local version=$1
 
