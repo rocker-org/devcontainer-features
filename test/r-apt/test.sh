eb9979157b395246523287444b05f8572ac62ba2
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-12-31 10:13:54 +0000

    add test of R package installation via R function

--- test/r-apt/test.sh
+++ test/r-apt/test.sh
@@ -8,5 +8,8 @@ source dev-container-features-test-lib
 # Feature-specific tests
 check "version" R -q -e "sessionInfo()"
 
+# Check package installation via R function
+R -q -e 'install.packages("glue")'
+
 # Report result
 reportResults
