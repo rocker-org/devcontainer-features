6659e173932f191f149530792fdd78d3943ed714
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-12-31 10:42:04 +0000

    fix test

--- test/r-apt/test.sh
+++ test/r-apt/test.sh
@@ -9,7 +9,8 @@ source dev-container-features-test-lib
 check "version" R -q -e "sessionInfo()"
 
 # Check package installation via R function
-R -q -e 'install.packages("glue")'
+R -q -e 'install.packages("R6")'
+check "R6" R -q -e 'names(installed.packages()[, 3])' | grep R6
 
 # Report result
 reportResults
