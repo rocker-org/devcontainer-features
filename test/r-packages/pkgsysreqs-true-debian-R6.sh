f16cf15695d2abca05e194d9f097a03edf28f129
eitsupi <50911393+eitsupi@users.noreply.github.com>
2023-01-02 02:48:13 +0000

    add more test

---
+++ test/r-packages/pkgsysreqs-true-debian-R6.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+check "R R6 package" R -q -e 'names(installed.packages()[, 3])' | grep R6
+
+# Report result
+reportResults
