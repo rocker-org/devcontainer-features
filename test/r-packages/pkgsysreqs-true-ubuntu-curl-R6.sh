29c4547a31a797f61ffda9d2619d59e2349fc8ca
eitsupi <50911393+eitsupi@users.noreply.github.com>
2023-01-02 11:44:43 +0000

    fix tests

--- test/r-packages/pkgsysreqs-true-ubuntu-curl-R6.sh
+++ test/r-packages/pkgsysreqs-true-ubuntu-curl-R6.sh
@@ -7,6 +7,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 check "R curl package" R -q -e 'names(installed.packages()[, 3])' | grep curl
+check "R R6 package" R -q -e 'names(installed.packages()[, 3])' | grep R6
 
 # Report result
 reportResults
