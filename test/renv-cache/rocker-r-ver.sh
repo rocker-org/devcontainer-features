43986d74c82e22ac4d5a8a4aa358c84721fefb59
eitsupi <ts1s1andn@gmail.com>
2023-01-07 23:43:52 +0900

    add tests

--- test/renv-cache/rocker-r-ver.sh
+++ test/renv-cache/rocker-r-ver.sh
@@ -7,6 +7,8 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 check "renv" R -q -e 'packageVersion("renv")'
+check "renv::install" R -q -e 'renv::install("jsonlite")'
+check "cache dir" find /renv/cache/*
 
 # Report result
 reportResults
