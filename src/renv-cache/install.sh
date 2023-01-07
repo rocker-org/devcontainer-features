e38340c8575c2f418573e38ec8a2b19da7a3795f
eitsupi <ts1s1andn@gmail.com>
2023-01-07 22:59:39 +0900

    add a new Feature, renv-cache

---
+++ src/renv-cache/install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+USERNAME=${USERNAME:-${_REMOTE_USER}}
+
+set -e
+
+if [ ! -x "$(command -v R)" ]; then
+    echo "(!) Cannot run R. Please install R before installing this Feature."
+    exit 1
+fi
+
+install_renv() {
+    if su "${USERNAME}" -c "R -s -e 'packageVersion(\"renv\")'" >/dev/null 2>&1; then
+        echo "Install renv R package from ..."
+        mkdir /tmp/renv-cache
+        cd /tmp/renv-cache
+        su "${USERNAME}" -c "R -s -e 'install.packages(\"renv\")'"
+        cd ..
+        rm -rf /tmp/renv-cache
+        rm -rf /tmp/Rtmp*
+    else
+        echo "renv R package is already installed. Skip renv installation..."
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+install_renv
+
+echo "Done!"
