1d8e63ae29de9b1988df9e71fb867f90af9512ee
eitsupi <50911393+eitsupi@users.noreply.github.com>
2023-01-08 15:10:56 +0900

    [renv-cache] Create cache dir in the image to fix permission error (#142)

--- src/renv-cache/install.sh
+++ src/renv-cache/install.sh
@@ -1,23 +1,43 @@
 #!/bin/sh
 
+RENV_PATHS_CACHE=${RENV_PATHS_CACHE:-"/renv/cache"}
+
 USERNAME=${USERNAME:-${_REMOTE_USER}}
 
 set -e
 
-if [ ! -x "$(command -v R)" ]; then
-    echo "(!) Cannot run R. Please install R before installing this Feature."
-    echo "    Skip installation..."
-    exit 0
-fi
+create_cache_dir() {
+    if [ -d "$1" ]; then
+        echo "Cache directory $1 already exists. Skip creation..."
+    else
+        echo "Create cache directory $1..."
+        mkdir -p "$1"
+    fi
+
+    if [ -z "$2" ]; then
+        echo "No username provided. Skip chown..."
+    else
+        echo "Change owner of $1 to $2..."
+        chown -R "$2:$2" "$1"
+    fi
+}
+
+check_r() {
+    if [ ! -x "$(command -v R)" ]; then
+        echo "(!) Cannot run R. Please install R before installing this Feature."
+        echo "    Skip installation..."
+        exit 0
+    fi
+}
 
 install_renv() {
-    if su "${USERNAME}" -c "R -s -e 'packageVersion(\"renv\")'" >/dev/null 2>&1; then
+    if su "$1" -c "R -s -e 'packageVersion(\"renv\")'" >/dev/null 2>&1; then
         echo "renv R package is already installed. Skip renv installation..."
     else
         echo "Install renv R package..."
         mkdir /tmp/renv-cache
         cd /tmp/renv-cache
-        su "${USERNAME}" -c "R -s -e 'install.packages(\"renv\")'"
+        su "$1" -c "R -s -e 'install.packages(\"renv\")'"
         cd ..
         rm -rf /tmp/renv-cache
         rm -rf /tmp/Rtmp*
@@ -26,6 +46,8 @@ install_renv() {
 
 export DEBIAN_FRONTEND=noninteractive
 
-install_renv
+create_cache_dir "${RENV_PATHS_CACHE}" "${USERNAME}"
+check_r
+install_renv "${USERNAME}"
 
 echo "Done!"
