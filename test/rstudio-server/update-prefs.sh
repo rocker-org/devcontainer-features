27f252fb0f12789839977ae7acc0e4eadd3e0493
eitsupi <50911393+eitsupi@users.noreply.github.com>
2023-09-02 16:15:39 +0900

    [rstudio-server] support for updating already existing prefs.json (#181)
    Add the ability to update `initial_working_directory` of
    `rstudio-prefs.json` using the `jq` command if `rstudio-prefs.json`
    already exists.

---
+++ test/rstudio-server/update-prefs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+export prefs_json_path=~/.config/rstudio/rstudio-prefs.json
+check "version" bash -c 'rstudio-server version'
+check "rstudio prefs 1" bash -c "jq -r '.initial_working_directory' ${prefs_json_path} | grep $(pwd)"
+check "rstudio prefs 2" bash -c "jq -r '.foo' ${prefs_json_path} | grep bar"
+
+# Report result
+reportResults
