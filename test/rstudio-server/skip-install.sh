c6aa45908845770d1b5de2c3457cf12dd8a9d3eb
eitsupi <50911393+eitsupi@users.noreply.github.com>
2023-09-02 17:10:57 +0900

    [rstudio-server] add a option to prevent reinstallation (#182)

--- test/rstudio-server/skip-install.sh
+++ test/rstudio-server/skip-install.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Feature-specific tests
-check "version" bash -c 'rstudio-server version | grep daily'
+check "version" bash -c "rstudio-server version | grep ${RSTUDIO_VERSION}"
 
 # Report result
 reportResults
