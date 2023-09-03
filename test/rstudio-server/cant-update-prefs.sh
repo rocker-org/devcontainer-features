608df22da0d7f5186be8486125d544b068f14a21
eitsupi <50911393+eitsupi@users.noreply.github.com>
2023-09-03 03:00:46 +0000

    fix test

--- test/rstudio-server/cant-update-prefs.sh
+++ test/rstudio-server/cant-update-prefs.sh
@@ -8,8 +8,8 @@ source dev-container-features-test-lib
 # Feature-specific tests
 export prefs_json_path=~/.config/rstudio/rstudio-prefs.json
 check "version" bash -c 'rstudio-server version'
-check "rstudio prefs 1" bash -c "jq -r '.initial_working_directory' ${prefs_json_path} | grep /home/rstudio"
-check "rstudio prefs 2" bash -c "jq -r '.foo' ${prefs_json_path} | grep bar"
+check "rstudio prefs 1" bash -c "cat ${prefs_json_path} | grep initial_working_directory | grep /home/rstudio"
+check "rstudio prefs 2" bash -c "cat ${prefs_json_path} | grep foo | grep bar"
 
 # Report result
 reportResults
