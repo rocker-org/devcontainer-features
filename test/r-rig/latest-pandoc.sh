a3a8f7b3f337ab7c2548256954c98dd3e362b972
eitsupi <50911393+eitsupi@users.noreply.github.com>
2024-04-26 14:54:02 +0000

    test: fix version pettern

--- test/r-rig/latest-pandoc.sh
+++ test/r-rig/latest-pandoc.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-LATEST_VERSION="$(git ls-remote --tags https://github.com/jgm/pandoc | grep -oP "[0-9]+\\.[0-9]+(\\.[0-9])*" | sort -V | tail -n 1)"
+LATEST_VERSION="$(git ls-remote --tags https://github.com/jgm/pandoc | grep -oP "[0-9]+\\.[0-9]+(\\.[0-9]+)*" | sort -V | tail -n 1)"
 
 # shellcheck source=/dev/null
 source dev-container-features-test-lib
