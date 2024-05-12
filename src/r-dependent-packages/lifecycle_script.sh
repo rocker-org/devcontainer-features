807d58991a34104e4448133d9f670eb3f88f3416
eitsupi <50911393+eitsupi@users.noreply.github.com>
2024-05-12 05:25:40 +0000

    test: update for temporary test

--- src/r-dependent-packages/lifecycle_script.sh
+++ src/r-dependent-packages/lifecycle_script.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-set -e
+# set -e
 
 echo "Install dependent R packages..."
 
-R -q -e \
-    'pak::repo_add(@REPOS@); pak::local_install_deps("@ROOT@", dependencies = trimws(unlist(strsplit("@DEPS@", ","))))'
+# R -q -e \
+#     'pak::repo_add(@REPOS@); pak::local_install_deps("@ROOT@", dependencies = trimws(unlist(strsplit("@DEPS@", ","))))'
 
 echo "Done!"
