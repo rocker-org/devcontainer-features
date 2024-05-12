2057c9314d1832b2318ab2537fbde20355780f76
eitsupi <50911393+eitsupi@users.noreply.github.com>
2024-05-12 16:53:50 +0900

    feat(r-dependent-packages): new feature `r-dependent-packages` (#226)

---
+++ src/r-dependent-packages/lifecycle_script.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+PKG_PACKAGE_CACHE_DIR=${PKG_PACKAGE_CACHE_DIR:-"/pak/cache"}
+
+set -e
+
+fix_permissions() {
+    local dir
+    dir="${1}"
+
+    if [ ! -w "${dir}" ]; then
+        echo "Fixing permissions of '${dir}'..."
+        sudo chown -R "$(id -u):$(id -g)" "${dir}"
+        echo "Done!"
+    else
+        echo "Permissions of '${dir}' are OK!"
+    fi
+}
+
+fix_permissions "${PKG_PACKAGE_CACHE_DIR}"
+
+echo "Install dependent R packages..."
+
+R -q -e \
+    'pak::repo_add(@REPOS@); pak::local_install_deps("@ROOT@", dependencies = trimws(unlist(strsplit("@DEPS@", ","))))'
+
+echo "Done!"
