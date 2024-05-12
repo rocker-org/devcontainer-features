504c3652fa0be2a6e265de5fb97e1e76ced32c72
eitsupi <50911393+eitsupi@users.noreply.github.com>
2024-05-12 05:14:49 +0000

    test: update for temporary test

--- src/r-dependent-packages/lifecycle_script.sh
+++ src/r-dependent-packages/lifecycle_script.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-set -e
-
-echo "Install dependent R packages..."
-
-R -q -e \
-    'pak::repo_add(@REPOS@);
-    pak::local_install_deps("@ROOT@", dependencies = trimws(unlist(strsplit("@DEPS@", ","))))'
-
-echo "Done!"
+echo "This lifecycle script is empty. Done nothing..."
+echo "root is @ROOT@"
